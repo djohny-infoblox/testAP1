7884a45198c05d0cc0f11cc3daffeb0165e1a920
Dhanson <74426802+djohny2020@users.noreply.github.com>
2020-11-24 23:53:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,17 +7,6 @@ import requests
 app = Flask(__name__)
 api = Api(app)
 
-# names = {
-#          "tim":{"age":12,"place":"dfsdf"},
-#          "dhan":{"age":144,"place":"zzz"},
-#          }
-
-# class helloworld(Resource):
-#     def get(self,name):
-#         return names[name]
-#     def post(self):
-#         return {"data" : "hello poasdsdsado"}
-
       
 class coredns(Resource):
     def get(self):
